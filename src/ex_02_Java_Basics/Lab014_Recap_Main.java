6120eabe5506b03feda38e7f12fa78e8cdb452ec
Nikhil28051991 <sonawanenikhil2805@gmail.com>
2026-08-02 00:00:13 +0530

    My 1  202Aug6, Please add 7 files

---
+++ src/ex_02_Java_Basics/Lab014_Recap_Main.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Recap_Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
+// Java could not have two main method in a Program but if user wants to create it then he will create but the Signature is different
+// i,e other than this       public static void main(String[]
