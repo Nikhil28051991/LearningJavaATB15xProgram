8b9b6427a2e91e29452658be596e665f4d471580
Nikhil28051991 <sonawanenikhil2805@gmail.com>
2026-08-05 20:10:55 +0530

    My 3 Aug 2026 code

--- src/ex_03_Literals/Lab35_String_Null.java
+++ src/ex_03_Literals/Lab35_String_Null.java
@@ -4,9 +4,15 @@ public class Lab35_String_Null {
     public static void main(String[] args) {
         String name = null; // This is the Example of Null Literal
 
-       // int a = null; // null can not be associated with integral values or null is not associated with Primitive data types
+         // int a = null; // null can not be associated with integral values or null is not associated with Primitive data types
+        // String can not be zero
+
+         String name1 = " ";
+
+        //How many Characters string have zero also
 
         System.out.println(name);
+        System.out.println(name1);
 
     }
 }
