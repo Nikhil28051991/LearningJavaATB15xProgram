8a7d477c3e1d3c4042ed89fc09c01f2e9b527ee5
Nikhil28051991 <sonawanenikhil2805@gmail.com>
2026-08-10 21:06:55 +0530

    My 6 Aug 2026 code

--- src/ex_06_Ternary_Operator/Lab066_TO_Even_Odd.java
+++ src/ex_06_Ternary_Operator/Lab066_TO_Even_Odd.java
@@ -4,7 +4,14 @@ public class Lab066_TO_Even_Odd {
     public static void main(String[] args) {
 
         int a = 14;
+
         String result = (a%2 ==0) ? "even" : "odd";
         System.out.println(result);
+
+        int b = 17;
+
+        String result1 = (b%2 ==0) ? "even" : "odd";
+        
+        System.out.println(result1);
     }
 }
