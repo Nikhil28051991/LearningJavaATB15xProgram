da9e3583dfa63cbaaf27edba20ef9300dc0bdad7
Nikhil28051991 <sonawanenikhil2805@gmail.com>
2026-08-21 00:13:06 +0530

    My 13 Aug 2026 code

---
+++ src/ex_12_DoWhile/Lab124_DoWhile.java
@@ -0,0 +1,12 @@
+package ex_12_DoWhile;
+
+public class Lab124_DoWhile {
+    public static void main(String[] args) {
+
+        int a = 1;
+        do {
+            System.out.println("Start the Program");
+            a++;
+        } while (a<10);
+    }
+}
