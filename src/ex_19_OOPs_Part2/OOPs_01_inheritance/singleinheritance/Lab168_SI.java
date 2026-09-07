2d706728f4212f0059b08e8d2f05d8bbcaafd424
Nikhil28051991 <sonawanenikhil2805@gmail.com>
2026-09-08 00:35:10 +0530

    My 27 Aug 2026 code

--- src/ex_19_OOPs_Part2/OOPs_01_inheritance/singleinheritance/Lab168_SI.java
+++ src/ex_19_OOPs_Part2/OOPs_01_inheritance/singleinheritance/Lab168_SI.java
@@ -3,10 +3,10 @@ package ex_19_OOPs_Part2.OOPs_01_inheritance.singleinheritance;
 public class Lab168_SI {
     public static void main(String[] args) {
 
-        Son pramod = new Son();
-        System.out.println(pramod.gold_f);
-        pramod.bhk2();
-        pramod.bhk3();
+        Son Nikhil = new Son();
+        System.out.println(Nikhil.gold_f);
+        Nikhil.bhk2();
+        Nikhil.bhk3();
 
         Father f1 = new Father();
         System.out.println(f1.gold_f);
